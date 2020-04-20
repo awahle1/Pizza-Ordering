685393c004e2eea3ef1736618a9bcb856184652b
awahle1 <46114721+awahle1@users.noreply.github.com>
2020-04-20 17:16:27 -0400

    All of the munu items added

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 from .models import Pizza
-
+from .models import Sub
+from .models import Pasta
+from .models import Salad
+from .models import DinnerPlatter
 # Register your models here.
 admin.site.register(Pizza)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(DinnerPlatter)
--- orders/models.py
+++ orders/models.py
@@ -65,3 +65,95 @@ class Pizza(models.Model):
     choices = [('Small', 'Small'), ('Large', 'Large')],
     default = 'Small',
     )
+
+class Sub(models.Model):
+    SUBOPTIONS=(
+    ('Cheese', 'Cheese'),
+    ('Italian', 'Italian'),
+    ('Ham + Cheese', 'Ham + Cheese'),
+    ('Meatball', 'Meatball'),
+    ('Tuna', 'Tuna'),
+    ('Turkey', 'Turkey'),
+    ('Chicken Parmigiana', 'Chicken Parmigiana'),
+    ('Eggplant Parmigiana', 'Eggplant Parmigiana'),
+    ('Steak', 'Steak'),
+    ('Steak + Cheese', 'Steak + Cheese'),
+    ('Sausage, Peppers & Onions', 'Sausage, Peppers & Onions'),
+    ('Hamburger', 'Hamburger'),
+    ('Cheeseburger', 'Cheeseburger'),
+    ('Fried Chicken', 'Fried Chicken'),
+    ('Veggie', 'Veggie'),
+    )
+
+    sub = models.CharField(
+    max_length = 30,
+    choices = SUBOPTIONS,
+    default='Cheese',
+    )
+
+    addmushrooms = models.CharField(
+    max_length = 3,
+    choices = [('Yes', 'Yes'), ('No', 'No')],
+    default='No',
+    )
+
+    addgreenpeppers = models.CharField(
+    max_length = 3,
+    choices = [('Yes', 'Yes'), ('No', 'No')],
+    default='No',
+    )
+
+    addonions = models.CharField(
+    max_length = 3,
+    choices = [('Yes', 'Yes'), ('No', 'No')],
+    default='No',
+    )
+
+    extracheese = models.CharField(
+    max_length = 3,
+    choices = [('Yes', 'Yes'), ('No', 'No')],
+    default='No',
+    )
+
+class Salad(models.Model):
+    SALADOPTIONS = (
+    ('Garden Salad', 'Garden Salad'),
+    ('Greek Salad', 'Greek Salad'),
+    ('Antipasto', 'Antipasto'),
+    ('Salad w/Tuna', 'Salad w/Tuna'),
+    )
+
+    salad = models.CharField(
+    max_length = 15,
+    choices=SALADOPTIONS,
+    default='Garden Salad',
+    )
+
+class Pasta(models.Model):
+    PASTAOPTIONS = (
+    ('Baked Ziti w/Mozzarella', 'Baked Ziti w/Mozzarella'),
+    ('Baked Ziti w/Meatballs', 'Baked Ziti w/Meatballs'),
+    ('Baked Ziti w/Chicken', 'Baked Ziti w/Chicken'),
+    )
+
+    salad = models.CharField(
+    max_length = 25,
+    choices=PASTAOPTIONS,
+    default='Baked Ziti w/Mozzarella',
+    )
+
+class DinnerPlatter(models.Model):
+    PLATTEROPTIONS = (
+    ('Garden Salad', 'Garden Salad'),
+    ('Greek Salad', 'Greek Salad'),
+    ('Antipasto', 'Antipasto'),
+    ('Baked Ziti', 'Baked Ziti'),
+    ('Meatball Parm', 'Meatball Parm'),
+    ('Chicken Parm', 'Chicken Parm'),
+    )
+
+    salad = models.CharField(
+    max_length = 20,
+    choices=PLATTEROPTIONS,
+    default='Baked Ziti',
+    )
